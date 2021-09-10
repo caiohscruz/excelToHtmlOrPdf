15fcce940937bca484d74103c1c0d19fe001d33e
caiohscruz <caiohscruz@hotmail.com>
2021-09-09 21:43:01 -0300

    feat process data to two-dimmensional array

---
+++ Processor.js
@@ -0,0 +1,16 @@
+class Processor{
+
+static Process(data){
+    var rows = data.split("\r\n")
+    var cells = []
+
+    rows.forEach(row =>{
+        var cols = row.split(",")
+        cells.push(cols)
+    })
+      
+    // console.log(cells) - test (nº2) to abstract content into two-dimmensional array
+}
+}
+
+module.exports = Processor
--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+const Processor = require("./Processor")
 var Reader = require("./Reader")
 
 var reader = new Reader()
 
+
 async function main(){
     var data = await reader.Read("./users.csv")
-    console.log(data)
+
+    // console.log(data) - test (nº1) to catch file content
+
+    var processedData = Processor.Process(data)
+
 }
 
 main()
